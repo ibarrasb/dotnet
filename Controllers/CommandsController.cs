c75b03c31b74e3b6255223e128ce9186f5269f60
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-08-17 20:39:52 -0500

    working api with postman on GET

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,9 +1,35 @@
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
 {
+    //api/commands
+    [Route("api/commands")]
+    [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+    // GET: api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            
+            //returns 200 OK
+            return Ok(commandItems);
 
+        }
+
+    //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+
+        }
     }
 }
